77a9a1e47aeb9e31f7bc1ca5883314cf7733eaa2
Joan Reig Roman <joan@MacBook-MacBook-Pro-de-Joan.local>
2020-11-14 12:33:08 +0100

    Restserver usuario

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,27 @@
 // =====================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =====================
+// Entorno
+// =====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =====================
+// Base de datos
+// =====================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:Abc.1234@cluster0.qg3tj.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
+
+
+
